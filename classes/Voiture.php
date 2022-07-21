15692e366985224501cc0da3a921fe22771daf3b
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-21 12:08:39 +0200

    creation de la methode magique __construct

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -4,28 +4,39 @@ class Voiture {
     private string $marque;
     private string $couleur;
     public int $nbrRoues = 4;
+
+    public function __construct(string $couleur)
+    {
+       $this->couleur = $couleur; 
+    }
     // Méthode pour récupérer la marque
-    public function getMarque(): string {
+    public function getMarque(): string 
+    {
         return $this->marque;
     }
     // Methode pour définir la marque 
-    public function setMarque(string $marque): void {
+    public function setMarque(string $marque): void
+    {
         $this->marque = $marque;
     }
 
-    public function getColor(): string {
+    public function getColor(): string 
+    {
         return $this->couleur;
     }
 
-    public function setColor(string $couleur): void {
+    public function setColor(string $couleur): void 
+    {
         $this->couleur = $couleur;
     }
 
-    public function demarrer() {
+    public function demarrer() 
+    {
         echo "<p>la voiture démarre</p>";
     }
 
-    public function afficherCouleur() {
+    public function afficherCouleur() 
+    {
         return "<p>La voiture est de couleur " .$this->couleur . "</p>";
     }
 }
--- index.php
+++ index.php
@@ -2,27 +2,27 @@
 
 require_once './classes/Voiture.php';
 
-$voiture1 = new Voiture();
+$voiture1 = new Voiture("blue");
 // $voiture1->couleur = "rouge";
 // $voiture1->marque = "bmw";
 // $couleurVoiture1 = $voiture1->afficherCouleur();
 
 $voiture1->setMarque("bmw");
 echo $voiture1->getMarque();
-$voiture1->setColor("blue");
+// $voiture1->setColor("blue");
 echo $voiture1->getcolor();
 
 
-$voiture2 = new Voiture();
-$voiture2->couleur = "vert";
+$voiture2 = new Voiture("red");
+// $voiture2->couleur = "vert";
 $voiture2->marque = "ferrari";
 $voiture2->demarrer();
 
 
-var_dump($voiture1);
-echo "<br />";
-var_dump($voiture2);
-echo $couleurVoiture1;
+// var_dump($voiture1);
+// echo "<br />";
+// var_dump($voiture2);
+// echo $couleurVoiture1;
 
 
 
