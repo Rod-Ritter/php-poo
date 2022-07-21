0ffe584491728f3cb1ac6ac3ba456abb6bcb035c
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-21 10:31:46 +0200

    instances différentes de voiture

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-require_once './classes/voiture';
+require_once './classes/Voiture.php';
+
+$voiture1 = new Voiture();
+$voiture1->couleur = "rouge";
+$voiture1->marque = "bmw";
+
+$voiture2 = new Voiture();
+$voiture2->couleur = "vert";
+$voiture2->marque = "ferrari";
+
+var_dump($voiture1);
+echo "<br>";
+var_dump($voiture2);
+
