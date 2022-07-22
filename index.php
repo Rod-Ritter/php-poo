a5dac0b30e08321c9063939af13f6494109a6b16
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-22 11:45:22 +0200

    mise en application de la fonction 'calculer energie cinetique

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ spl_autoload_register(function ($className) {
     require_once './classes/' . $className . '.php';
 });
 
+
+
 $voiture1 = new Voiture("orange");
 $voiture1->setMarque("BMW");
-var_dump($voiture1);
+$voiture1->setVitesse((50));
+$voiture1->setMasse((1200));
+echo $voiture1->calculerEnergieCinetique() . " Joules<br />";
+$voiture1->setVitesse((60));
+echo $voiture1->calculerEnergieCinetique() . " Joules<br />";
+
